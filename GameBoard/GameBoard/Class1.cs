57fd09929a91a83af6e969376628121b20e01861
Naaaske <thomasstubkjaer@gmail.com>
2019-03-14 12:17:27 +0100

    Dice
    its dice time madafaka

---
+++ GameBoard/GameBoard/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBoard
+{
+  class Dice
+  {
+    Random rnd = new Random();
+
+    public int Roll()
+    {
+      return rnd.Next(6) + 1;
+    }
+  }
+}
